55c2e7c266c1262e489eff467b51360f1e429c5d
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-03-04 11:28:16 -0600

    "Primera sesión de app con código"

--- app/src/main/java/com/marcoslopez7/cosascuriosas/FunFactsActivity.java
+++ app/src/main/java/com/marcoslopez7/cosascuriosas/FunFactsActivity.java
@@ -2,12 +2,31 @@ package com.marcoslopez7.cosascuriosas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+
+    //Miembros de la clase
+    private TextView factText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+        //Aquí comienza a correr nuestro código
+        factText = (TextView) findViewById(R.id.datoCurioso); //Apuntamos al text view de la interfaz
+        button = (Button) findViewById(R.id.boton);//Apuntamos al botón de la interfaz
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Aquí le damos funcionalidad al botón
+                String fact = "El Real Madrid es el mejor de la historia, porque tiene 10 champions, y el barca no";
+                factText.setText(fact);
+            }
+        });
     }
 }
